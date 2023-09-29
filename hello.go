5c2bcced533458531a955ac3f5dc6173154c6762
carvalhocaio <caiocarvalho.py@gmail.com>
2023-09-29 12:59:15 -0400

    tipos de variaveis

--- hello.go
+++ hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var nome string = "Hailey"
+	var idade int
+	var versao float32 = 1.1
+	fmt.Println("Hello,", nome, "sue idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
 }
