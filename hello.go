9f4f8992de7d8ab51526f4af193101e82e5d02d2
carvalhocaio <caiocarvalho.py@gmail.com>
2023-09-29 14:44:08 -0400

    exibindo o menu

--- hello.go
+++ hello.go
@@ -4,8 +4,16 @@ import "fmt"
 
 func main() {
 	nome := "Hailey"
-	idade := 23
 	var versao float32 = 1.1
-	fmt.Println("Hello,", nome, "sue idade é", idade)
+	fmt.Println("Hello,", nome)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O comando escolhido foi", comando)
 }
