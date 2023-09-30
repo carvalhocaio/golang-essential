fcee019721e843e9508d3a6407a330186fc02878
carvalhocaio <caiocarvalho.py@gmail.com>
2023-09-30 17:38:26 -0400

    iniciando o monitoramento

--- hello.go
+++ hello.go
@@ -13,7 +13,7 @@ func main() {
 
 	switch comando {
 	case 1:
-		fmt.Println("Monitorando...")
+		iniciarMonitoramento()
 	case 2:
 		fmt.Println("Exibindo Logs...")
 	case 0:
@@ -44,4 +44,11 @@ func leComando() int {
 	fmt.Println("O comando escolhido foi", comandoLido)
 
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+
+	site := "https://www.alura.com.br"
+	// resp, err := http.Get(site)
 }
