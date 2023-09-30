a124bd238a8446b930e0a51570f4435f595bc359
carvalhocaio <caiocarvalho.py@gmail.com>
2023-09-30 17:30:42 -0400

    introducao as funcoes

--- hello.go
+++ hello.go
@@ -1,21 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	nome := "Hailey"
-	var versao float32 = 1.1
-	fmt.Println("Hello,", nome)
-	fmt.Println("Este programa está na versão", versao)
+	exibeIntroducao()
+	exibeMenu()
 
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair")
-
-	var comando int
-	fmt.Scan(&comando)
-
-	fmt.Println("O comando escolhido foi", comando)
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -24,7 +18,30 @@ func main() {
 		fmt.Println("Exibindo Logs...")
 	case 0:
 		fmt.Println("Saindo...")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço esse comando")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	nome := "Hailey"
+	var versao float32 = 1.1
+	fmt.Println("Olá,", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
