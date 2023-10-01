2c57c81a3186b8451802ed930b4287a19b35ce24
carvalhocaio <caiocarvalho.py@gmail.com>
2023-10-01 00:14:11 -0400

    testando multiplas vezes

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 	exibeIntroducao()
@@ -46,6 +50,7 @@ func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
 	return comandoLido
 }
@@ -59,13 +64,24 @@ func iniciarMonitoramento() {
 		"https://caelum.com.br",
 	}
 
-	for _, site := range sites {
-		resp, _ := http.Get(site)
-
-		if resp.StatusCode == 200 {
-			fmt.Println("Site:", site, "foi carregado com sucesso!")
-		} else {
-			fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	for i := 0; i < monitoramentos; i++ {
+		for _, site := range sites {
+			fmt.Println("Testando site:", site)
+			testaSite(site)
 		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println(">>", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println(">>", site, "está com problemas. Status Code:", resp.StatusCode)
 	}
 }
