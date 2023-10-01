af3f3ac1fe22e897e3c18330b508f341b2408c78
carvalhocaio <caiocarvalho.py@gmail.com>
2023-09-30 20:44:43 -0400

    monitorando os nossos sites

--- main.go
+++ main.go
@@ -8,27 +8,30 @@ import (
 
 func main() {
 	exibeIntroducao()
-	exibeMenu()
 
-	comando := leComando()
+	for {
+		exibeMenu()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço esse comando")
-		os.Exit(-1)
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço esse comando")
+			os.Exit(-1)
+		}
 	}
 }
 
 func exibeIntroducao() {
 	nome := "Hailey"
-	var versao float32 = 1.1
+	var versao float32 = 1.2
 	fmt.Println("Olá,", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
@@ -50,7 +53,12 @@ func leComando() int {
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
 
-	site := "https://www.alura.com.br"
+	site := "https://random-status-code.onrender.com"
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
